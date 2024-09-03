69a6bda4b1ef7af2dcc312b9c63bcc544fca0f32
Ofboir <ofboir@yahoo.fr>
2024-09-03 18:34:03 +0200

    First version

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # ShotcutChapters2OGM
-Simple script used to convert chapters created in Shotcut to OGM format
+Simple script used to convert chapters created in [Shotcut](https://www.shotcut.org/) to OGM format.
+The output file is created only with [MKVToolNix](MKVToolNix) in mind. It has not been tested with other tools.
+
+## Usage
+ShotcutChapters2OGM.py [-h] [-o OUTPUT] [-f FPS] input
+
+Converts chapters exported from Shotcut (txt) to OGM format (txt).
+
+positional arguments:
+  input                 Input file.
+
+options:
+  -h, --help            show this help message and exit
+  -o OUTPUT, --output OUTPUT
+                        Output file.
+  -f FPS, --fps FPS     Video FPS (default=30).
+
+## How to use with Shotcut and MKVToolNix
+
+### Create chapters in Shotcut
+Place markers where you would like your chapters and name them the way you like.
+Then use File -> Export -> Markers as chapters...
+
+### Add chapters to video in MKVToolNix
+When muxing your video in MKV, go into the "Target" tab. In "Chapters", "Chapter file", select the file converted with ShotcutChapters2OGM.
---
+++ ShotcutChapters2OGM.py
@@ -0,0 +1,84 @@
+from pathlib import Path
+import io
+import re
+import argparse
+
+class ShotcutChapters2OGM:
+
+    def __init__(self, inputfile, fps, outputfile):
+        self.inputfile = Path(inputfile)
+        if outputfile is None:
+            self.outputfile = self.inputfile.with_suffix(".ogm.txt")
+        else:
+            self.outputfile = Path(outputfile)
+        self.fps = fps
+        self.chapternumber = 1
+        self.convertfile()
+
+    def convertfile(self):
+        input = io.open(self.inputfile, mode="r", encoding="utf-8")
+
+        self.outputfile.parent.mkdir(parents = True, exist_ok = True)
+        output = io.open(self.outputfile, mode="w", encoding="ansi")
+
+        while True:
+            content=input.readline()
+            if not content:
+                break
+            output.writelines(self.convertline(content))
+            self.chapternumber += 1
+            
+        output.close()
+        input.close()
+
+        print("File written : {}".format(self.outputfile.resolve()))
+    
+    def convertline(self, text):
+        converted = []
+
+        # Input format : {Timestamp} {Title}
+        match = re.match("([\d:]+)\s(.+)", text, re.UNICODE)
+        if (match):
+            numberinstring = f'{self.chapternumber:02d}'
+
+            # Timestamp format is HH:MM:SS:FF (FF = frames)
+            timestamp = match.group(1)
+            timestampmatch = re.fullmatch("(\d{2}:\d{2}):(\d{2}):(\d{2})", timestamp)
+            if timestampmatch:
+                hhmm = timestampmatch.group(1)
+                seconds = int(timestampmatch.group(2))
+                frames = int(timestampmatch.group(3))
+                output_seconds = seconds + frames / self.fps
+                timestamp = "{}:{:012.9f}".format(hhmm, output_seconds)
+            else:
+                # Timestamp format is HH:MM:SS
+                timestampmatch = re.fullmatch("\d{2}:\d{2}:\d{2}", timestamp)
+                if timestampmatch:
+                    timestamp = "{}.{:09d}".format(timestamp, 0)
+                else:
+                    # Timestamp format is HH:MM
+                    timestampmatch = re.fullmatch("\d{2}:\d{2}", timestamp)
+                    if timestampmatch:
+                        timestamp = "{}:{:012.9f}".format(timestamp, 0)
+
+            converted.append("CHAPTER{}={}".format(numberinstring, timestamp))
+            converted.append("\n")
+            converted.append("CHAPTER{}NAME={}".format(numberinstring, match.group(2)))
+            converted.append("\n")
+        else:
+            print("Unknown format: {}".format(text))
+
+        return converted
+
+###
+
+print("ShotcutChapters2OGM")
+
+# Arguments
+parser = argparse.ArgumentParser(description="Converts chapters exported from Shotcut (txt) to OGM format (txt).")
+parser.add_argument("-o", "--output", help="Output file.", type=str, required=False)
+parser.add_argument("-f", "--fps", help="Video FPS (default=30).", type=int, default=30)
+parser.add_argument("input", help="Input file.", type=str)
+args = parser.parse_args()
+
+ShotcutChapters2OGM(args.input, args.fps, args.output)
